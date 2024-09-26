6360bd7bc952b56fba614f7cb45ff2703a3c588e
Amilkar Sanchez <amilkaralan@gmail.com>
2024-09-25 23:02:09 -0600

    cambios[informacion extra]

--- src/components/BarraLateral.jsx
+++ src/components/BarraLateral.jsx
@@ -12,45 +12,56 @@ const BarraLateral = () => {
     if (id) {
       dispatch(getPokemon(id));
       console.log(id);
-      
+
     }
-  }, [dispatch, id]);
+  }, [ dispatch, id ]);
 
   useEffect(() => {
     if (pokemon && pokemon.species?.url) {
       dispatch(fetchExtraInfo(pokemon.species.url));
     }
-  }, [dispatch, pokemon]);
+  }, [ dispatch, pokemon ]);
+
+  const flavorTextInSpanish = pokeExtra?.flavor_text_entries.find(
+    (entry) => entry.language.name === "es"
+  );
 
   return (
-    <aside className="h-full w-full flex flex-col bg-blue-200">
-      {id && pokemon ? (
-        <div className="">
-          {/* Imagen del Pokémon */}
-          <div className="w-40 h-40 rounded-full">
+    <aside className="h-full w-full flex flex-col content-center bg-white">
+      { id && pokemon ? (
+        <div className="flex flex-col items-center">
+          {/* Imagen del Pokémon */ }
+          <div className="w-40 h-40 rounded-full relative">
             <img
-              className="object-cover h-full w-full"
-              src={pokemon.sprites?.other?.home?.front_default}
-              alt={pokemon.name}
+              className="object-cover h-full w-full absolute bottom-14"
+              src={ pokemon.sprites?.other?.home?.front_default }
+              alt={ pokemon.name }
             />
           </div>
 
-          {/* Información del Pokémon */}
-          <div className="">
+          {/* Información del Pokémon */ }
+          <div className="flex flex-col items-center">
             <p>
               <span>#</span>
-              {`${pokemon.id?.toString().padStart(4, "0")}`}
+              { `${pokemon.id?.toString().padStart(4, "0")}` }
             </p>
-            <h3>{pokemon.name}</h3>
-            {/* Información extra */}
-            {pokeExtra && <p>{pokeExtra.flavor_text_entries[0].flavor_text}</p>}
+            <h3>{ pokemon.name }</h3>
+            { pokeExtra && (
+              <div>
+              {flavorTextInSpanish ? (
+                <p>{flavorTextInSpanish.flavor_text}</p>
+              ) : (
+                <p>No se encontró una descripción en español.</p>
+              )}
+            </div>
+            ) }
           </div>
         </div>
       ) : (
         <div>
           <p>Busca o selecciona un Pokémon</p>
         </div>
-      )}
+      ) }
     </aside>
   );
 };
